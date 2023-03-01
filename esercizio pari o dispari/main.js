223a4e0ef48b963796f07b21731d3cef685470d2
albertogioia93 <albertogioia@hotmail.it>
2023-03-01 21:13:58 +0100

    mi sono bloccato

--- esercizio pari o dispari/main.js
+++ esercizio pari o dispari/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// creare una funzione che verifica se il numero è pari o dispari
+// se è pari RETURNE TRUE
+// altrimenti RETURNE FALSE
+
+function pariDispari(num){
+    if(num % 2 === 0){
+        return true;
+    } else {
+        return 'dispari'
+    }
+    // oppure senza }ELSE, sotto la graffa di IF, scrivo direttamente RETURN FALSE o RETURN 'DISPARI'
+}
+
+// const numeroUtente = Number(prompt('inserisci un numero')); 
+// anche se scrivo una parola mi da comunque pari/pariDispari. chiedere a Gaetano
+// console.log(pariDispari(numeroUtente));
+
+
+// PER GENERARE UN NUMERO RANDOM USANDO UNA FUNZIONE
+
+// function nRandom(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) ) + min;
+//   }
+// console.log(nRandom(1,6));
+
+const numeroUtente = Number(prompt('scegli un numero da 1 a 5'));
+
+if(numeroUtente > 5){
+    Number(prompt('scegli un numero da 1 a 5'));
+} else {
+    console.log(numeroUtente);
+}
+// console.log(numeroUtente);
